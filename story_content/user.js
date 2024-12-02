c16159656bdc4d4eafdb37d98b7636d445b87cb0
Liawidia <liawidiastuti19@guru.sd.belajar.id>
2024-12-02 21:29:44 +0800

    upload

---
+++ story_content/user.js
@@ -0,0 +1,221 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "67vitLsZyrV":
+        Script1();
+        break;
+      case "67MPGlIpnKo":
+        Script2();
+        break;
+      case "68fwlKbDV28":
+        Script3();
+        break;
+      case "5rVzsh6X948":
+        Script4();
+        break;
+      case "5bLWNTh9zeG":
+        Script5();
+        break;
+      case "6ZvG0Kt8sia":
+        Script6();
+        break;
+      case "6NTtAISTgDa":
+        Script7();
+        break;
+      case "6qPq3zFcOn6":
+        Script8();
+        break;
+      case "5aCrG49rbav":
+        Script9();
+        break;
+      case "6IWULspiCHz":
+        Script10();
+        break;
+      case "6Je0MhKYEwF":
+        Script11();
+        break;
+      case "6CT5bqGFjH2":
+        Script12();
+        break;
+      case "5Xue0AfA1lf":
+        Script13();
+        break;
+      case "6Mjs7GVlVUK":
+        Script14();
+        break;
+      case "6i8MzFxGIBO":
+        Script15();
+        break;
+      case "5weEUpod8Ww":
+        Script16();
+        break;
+      case "6ggOWUS92p3":
+        Script17();
+        break;
+      case "5pGtocIucA4":
+        Script18();
+        break;
+  }
+}
+
+function Script1()
+{
+  //load the scripts dynamically into the head of the document
+
+function add_line() {
+    var line = document.createElement("audio");
+    var head=document.getElementsByTagName('body')[0];
+    line.type = "audio/mp3";
+    line.src="";
+    line.id="bgSong" ;
+	line.autoplay = true;
+	line.loop = true;
+    head.appendChild(line);
+}
+
+//but we only want to add these once!
+if(document.getElementById('bgSong')==null){
+	add_line();
+var audio = document.getElementById('bgSong');
+audio.volume = 0.5;
+}
+
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.play();
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.pause();
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.play();
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.pause();
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.play();
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.pause();
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.play();
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.pause();
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.play();
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.pause();
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.play();
+}
+
+function Script13()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.pause();
+}
+
+function Script14()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.play();
+}
+
+function Script15()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.pause();
+}
+
+function Script16()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.play();
+}
+
+function Script17()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.pause();
+}
+
+function Script18()
+{
+  var audio = document.getElementById('bgSong');
+audio.src="story_content/external_files/happy.mp3";
+audio.load();
+audio.play();
+}
+
